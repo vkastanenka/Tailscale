8fb10e8dbbb533557d9a9f7fb86c2bbb745fc5b1
Victoria Kastanenka <vkastanenka@gmail.com>
2024-09-07 15:58:50 -0400

    created how it works section

--- app/page.tsx
+++ app/page.tsx
@@ -242,5 +242,58 @@ const Hero = ({ ...props }): JSX.Element => {
 }
 
 const HowItWorks = ({ ...props }): JSX.Element => {
-  return <section {...props}></section>
+  return (
+    <section
+      className={cx('overflow-hidden', 'p-top-115', 'p-bottom-175')}
+      {...props}
+    >
+      <div className={cx('space-y-8', 'md:!space-y-10', 'container')}>
+        {/* TODO: Create element animation */}
+        <div className={cx('h-full', 'w-full')}>IP Address Element</div>
+        <div className={cx('text-center')}>
+          <h2
+            className={cx(
+              't-h2',
+              'text-black',
+              'mb-3',
+              'md:mb-6',
+              'mx-auto',
+              'max-w-[782px]'
+            )}
+          >{`Tailscale makes secure networking easy`}</h2>
+          <div
+            className={cx(
+              't-b20',
+              'text-body-black',
+              'mg-5',
+              'lg:mg-[65px]',
+              'mx-auto',
+              'max-w-[530px]'
+            )}
+          >
+            <div className={cx('content-prose')}>
+              <p>{`Achieve point-to-point network connectivity that enforces least privelege.`}</p>
+            </div>
+          </div>
+          <div className={cx('w-full', 'xs:w-auto', 'flex', 'justify-center')}>
+            <div
+              className={cx(
+                'flex',
+                'w-full',
+                'flex-col',
+                'gap-y-4',
+                'xs:w-auto',
+                'xs:flex-row',
+                'xs:items-center',
+                'xs:space-x-5',
+                'md:space-x-[30px]'
+              )}
+            >
+              <Button variant="underlined">{`How it works`}</Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
