da4fe8c87ddf18ca2876e44b7d20adb192ccea91
Victoria Kastanenka <vkastanenka@gmail.com>
2024-09-08 03:17:17 -0400

    completed security refactor

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,7 @@ export default function Home() {
       <Homelab />
       {/* <Testimonials /> */}
       <Benefits />
-      {/* <Security /> */}
+      <Security />
       {/* <Documentation /> */}
     </main>
   )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,8 +22,9 @@ const config: Config = {
       ],
     },
     screens: {
-      'xl': '1280px',
-      'lg': '1024px',
+      xl: '1280px',
+      lg: '1024px',
+      'md-large': '890px',
       md: '768px',
       sm: '640px',
       xs: '480px',
